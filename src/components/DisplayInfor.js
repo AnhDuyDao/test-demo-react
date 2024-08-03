277f63b7bda86a3e5dda25932ddac5d9112feb68
Duy Anh <duyanhdao297@gmail.com>
2024-08-03 17:27:38 +0700

    Session 23

---
+++ src/components/DisplayInfor.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class DisplayInfor extends React.Component {
+    render() {
+        //destructuring array/object
+        const {age, name} = this.props;// object
+        console.log(this.props)
+        return (
+            <div>
+                <div>My name's {name}</div>
+                <div>My age's {age}</div>
+            </div>
+        )
+    }
+}
+
+export default DisplayInfor;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,12 +2,19 @@
 //function component
 import React from "react";
 import UserInfor from "./UserInfor";
+import DisplayInfor from "./DisplayInfor";
+
 class MyComponent extends React.Component {
     //JSX 
     render(){
+        const myInfor = ['ab', 'c', 'c'];
         return (
             <div>
                 <UserInfor/>
+                <br/><br/>
+                <DisplayInfor name="Hoi Dan IT" age="27" />
+                <hr/>
+                <DisplayInfor name="Duy Anh" age={26} myInfor={myInfor}/>
             </div>
         );
     }
