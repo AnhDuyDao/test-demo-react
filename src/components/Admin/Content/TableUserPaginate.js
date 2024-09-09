6a11b1126a54e2d30c54a05a7a0817854efe6376
Duy Anh <duyanhdao297@gmail.com>
2024-09-09 15:50:28 +0700

    Session 69

--- src/components/Admin/Content/TableUserPaginate.js
+++ src/components/Admin/Content/TableUserPaginate.js
@@ -10,6 +10,7 @@ const TableUserPaginate = (props) => {
 
    const handlePageClick = (event) => {
       props.fetchListUsersWithPaginate(+event.selected + 1);
+      props.setCurrentPage(+event.selected + 1);
       console.log(`User requested page number ${event.selected}`);
 
    };
@@ -50,7 +51,7 @@ const TableUserPaginate = (props) => {
                }
                {listUsers && listUsers.length === 0 &&
                   <tr>
-                     <td colSpan={'4'}>Not found data</td>
+                     <td colSpan={'5'}>Not found data</td>
                   </tr>
                }
 
@@ -76,6 +77,7 @@ const TableUserPaginate = (props) => {
                containerClassName="pagination"
                activeClassName="active"
                renderOnZeroPageCount={null}
+               forcePage={props.currentPage - 1}
             />
          </div>
 
